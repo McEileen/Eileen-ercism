04c03d3d09d9671966c0514a3e6acf420a71b73f
McEileen <eileenm@gmail.com>
2016-05-04 10:14:52 -0500

    upload java hello_word

--- java/hello-world/src/main/java/HelloWorld.java
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -1,2 +1,10 @@
 public class HelloWorld {
+    public static String hello(String name) {
+        if(name!= null) {
+          return "Hello, " + name + "!";
+        }
+        else {
+          return "Hello, World!";
+        }
+    }
 }
